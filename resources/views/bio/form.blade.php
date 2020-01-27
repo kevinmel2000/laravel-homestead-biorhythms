de1a4f80ed7b8d0cfd1a002ce680b83d61b13fed
nordenm <nordenmaachi@outlook.com>
2020-01-27 17:01:17 +0100

    Initial commit

---
+++ resources/views/bio/form.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+
+@section('titlePage', 'Calcula el teu biorritme')
+
+@section('header')
+<div>
+    
+    <blockquote class="blockquote text-center">
+        <p>Que són els Biorritmes?</p>
+        <p>Els biorritmes són cicles diaris i recurrents de fenòmens fisiològics que reflecteixen 
+        les actituds de les persones, sentiments o estats d'ànim.</p>
+        <p>Aquesta teoria la va iniciar el metge alemany Wilhem Fliess després de 
+        recopilar dades de personals com el nombre de dies viscuts, cicles de malalties, 
+        periodicitat d'accidents, etc. Posteriorment seus continuadors van destacar que 
+        un nombre important de morts es produïen quan els tres cicles, de 23, 28 i 33 dies, 
+        coincidien en un dia crític. D'aquesta manera van determinar que hi havia una propensió 
+        als accidents o malalties major en alguns dies determinats.</p>
+        <footer class="blockquote-footer">biorritmes.norden</cite></footer>
+    </blockquote>
+</div>
+
+<br/>
+
+@endsection
+
+@section('content')
+
+<form action="store" method="get">
+    <div class="form-group">
+        <label for="webId">Nom d'usuari</label>
+        <input type="text" class="form-control" id="webId" name="webId">
+    </div>
+    <div class="form-group">
+        <label for="webUrl">Data de naixement</label>
+        <input type="date" class="form-control" id="webUrl" name="webUrl">
+    </div>
+    <button type="submit" class="btn btn-secondary">Calcular</button>
+</form>
+
+@endsection
+
+@section('footer')
+
+@endsection
---
+++ resources/views/bio/result.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('titlePage', 'Resultat del càlcul')
+
+@section('header')
+<h1>Usuari {{$userName}} amb data de naixement {{$birthDate}}</h1>
+@endsection
+
+@section('content')
+
+
+
+@endsection
+
+@section('footer')
+<h3>Això és el footer de App Link</h3>
+@endsection
