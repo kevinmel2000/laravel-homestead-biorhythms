ea5654e815a9a30fea10bef7963e31b40c38a706
nordenm <nordenmaachi@outlook.com>
2020-02-02 14:21:58 +0100

    Project finished

--- app/Http/Controllers/CalculBio.php
+++ app/Http/Controllers/CalculBio.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Exception;
 
 class CalculBio extends Controller
 {
@@ -34,7 +35,56 @@ class CalculBio extends Controller
      */
     public function store(Request $request)
     {
-        return view('bio.result', ['userName' => $request->input('userName'), 'birthDate' => $request->input('birthDate')]);
+        if (empty($request->input('birthDate')) || empty($request->input('userName'))) {
+            return view('bio.error', ['error' => 'ERROR! Introdueix les dades correctement']);
+        } else {
+            $daysDifference = $this->calculateDaysBetweenDates(time(), strtotime($request->input('birthDate')));
+            $physical = $this->calculateBiorythm($daysDifference, 23);
+            $emotional = $this->calculateBiorythm($daysDifference, 28);
+            $intellectual = $this->calculateBiorythm($daysDifference, 33);
+
+            $physicalPercentage = round($physical, 0);
+            $emotionalPercentage = round($emotional, 0);
+            $intellectualPercentage = round($intellectual, 0);
+
+            $birthDate = date("d/m/Y", strtotime($request->input('birthDate')));
+
+            return view('bio.result', [
+                'userName' => $request->input('userName'),
+                'birthDate' => $birthDate,
+                'physical' => $physical,
+                'physicalPercentage' => $physicalPercentage,
+                'emotional' => $emotional,
+                'emotionalPercentage' => $emotionalPercentage,
+                'intellectual' => $intellectual,
+                'intellectualPercentage' => $intellectualPercentage
+            ]);
+        }
+    }
+
+    public function calculateDaysBetweenDates($date1, $date2)
+    {
+        try {
+            return round(($date1 - $date2) / (60 * 60 * 24)); // Diferència de dies
+        } catch (Exception $e) {
+            return view('bio.error', ['error' => 'ERROR! Error en el càlcul']);
+        }
+    }
+
+    public function calculateBiorythm($daysDifference, $cicle)
+    {
+        try {
+            return ((sin(($daysDifference / $cicle) * 2 * pi()) + 1) * 100) / 2;
+        } catch (Exception $e) {
+            return view('bio.error', ['error' => 'ERROR! Error en el càlcul']);
+        }
+    }
+
+    public function console_log($data)
+    {
+        echo '<script>';
+        echo 'console.log(' . json_encode($data) . ')';
+        echo '</script>';
     }
 
     /**
